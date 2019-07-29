9263016702518ab4b89e0afbcc774403e05ea71c
cyq <873002792@qq.com>
2019-07-29 11:31:20 +0800

    修改返回结果的接口回调

--- okhttputils/src/main/java/com/cyq/okhttputils/OKHttpUtils.java
+++ okhttputils/src/main/java/com/cyq/okhttputils/OKHttpUtils.java
@@ -3,8 +3,10 @@ package com.cyq.okhttputils;
 import android.text.TextUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -17,8 +19,21 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OKHttpUtils {
-    //设置设置超时时间和缓存
-    private static OkHttpClient client = new OkHttpClient.Builder().build();
+    private OkHttpClient client;
+    private static OKHttpUtils okHttpUtils;
+
+    public OKHttpUtils() {
+        client = new OkHttpClient.Builder()
+                .connectTimeout(12000, TimeUnit.SECONDS)
+                .build();
+    }
+
+    public static OKHttpUtils getInstance(){
+        if (okHttpUtils == null) {
+            okHttpUtils = new OKHttpUtils();
+        }
+        return okHttpUtils;
+    }
 
     /**
      * 同步调用，需要开启子线程
@@ -26,14 +41,18 @@ public class OKHttpUtils {
      * 另外可以根据response.code()获取返回的状态码。
      * @param url
      */
-    public void httpSyncGet(String url,SyncCallback callback) {
+    public void httpSyncGet(String url, HttpCallback callback) {
         this.callback = callback;
         try {
             Request request = new Request.Builder().url(url).get().build();
             Call call = client.newCall(request);
             Response response = call.execute();
 
-            this.callback.onCompleted(response);//回调请求结果
+            if (response.isSuccessful()) {
+                this.callback.onResponse(call,response);//回调请求结果
+            }else{
+                this.callback.onFailure(call,new IOException(response.message()));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,11 +64,12 @@ public class OKHttpUtils {
      * 另外可以根据response.code()获取返回的状态码。
      * @param url
      */
-    public void httpAsynGet(String url,Callback callback) {
+    public void httpAsynGet(String url, HttpCallback callback) {
+        this.callback = callback;
         try {
             Request request = new Request.Builder().url(url).get().build();
             Call call = client.newCall(request);
-            call.enqueue(callback);
+            call.enqueue(myCallback);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,7 +82,8 @@ public class OKHttpUtils {
      * @param map
      * @param callback
      */
-    public static void httpAsynGet(String url, Map<String, String> map, Callback callback) {
+    public void httpAsynGet(String url, Map<String, String> map, HttpCallback callback) {
+        this.callback = callback;
         try {
             StringBuffer sb = new StringBuffer();
             //拼接URL的参数
@@ -84,7 +105,7 @@ public class OKHttpUtils {
             //请求调用
             Request request = new Request.Builder().get().url(resultUrl).build();
             Call call = client.newCall(request);
-            call.enqueue(callback);
+            call.enqueue(myCallback);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -96,7 +117,8 @@ public class OKHttpUtils {
      * @param map
      * @param callback
      */
-    public static void httpAsynPost(String url, Map<String, String> map,Callback callback){
+    public void httpAsynPost(String url, Map<String, String> map, HttpCallback callback){
+        this.callback = callback;
         try {
             FormBody.Builder builder = new FormBody.Builder();
 
@@ -109,7 +131,7 @@ public class OKHttpUtils {
             FormBody formBody = builder.build();
             Request request = new Request.Builder().post(formBody).url(url).build();
             Call call = client.newCall(request);
-            call.enqueue(callback);//异步请求方式
+            call.enqueue(myCallback);//异步请求方式
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -124,7 +146,8 @@ public class OKHttpUtils {
      * @param filePath 路径
      * @param callback
      */
-    public static void httpAsynPost(String url, Map<String, String> map,String mediaType,String fileName,String filePath,Callback callback){
+    public void httpAsynPost(String url, Map<String, String> map, String mediaType, String fileName, String filePath, HttpCallback callback){
+        this.callback = callback;
         try {
             MultipartBody.Builder builder = new MultipartBody.Builder();
             if (map != null && map.isEmpty()) {
@@ -138,7 +161,7 @@ public class OKHttpUtils {
 
             Request request = new Request.Builder().post(body).url(url).build();
             Call call = client.newCall(request);
-            call.enqueue(callback);
+            call.enqueue(myCallback);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -153,7 +176,8 @@ public class OKHttpUtils {
      * @param paths 路径集合
      * @param callback
      */
-    public static void httpAsynPost(String url, Map<String, String> map, String mediaType, String fileKey, List<String> paths, Callback callback){
+    public void httpAsynPost(String url, Map<String, String> map, String mediaType, String fileKey, List<String> paths, HttpCallback callback){
+        this.callback = callback;
         try {
             MultipartBody.Builder builder = new MultipartBody.Builder();
             if (map != null && map.isEmpty()) {
@@ -172,18 +196,31 @@ public class OKHttpUtils {
 
             Request request = new Request.Builder().post(body).url(url).build();
             Call call = client.newCall(request);
-            call.enqueue(callback);
+            call.enqueue(myCallback);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private SyncCallback callback;
-    public interface SyncCallback{
-        void onCompleted(Response response);
+    private Callback myCallback = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            if (callback!=null)callback.onFailure(call,e);
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            if (callback!=null)callback.onResponse(call,response);
+        }
+    };
+
+    private HttpCallback callback;
+    public interface HttpCallback {
+        void onFailure(Call call, IOException e);
+        void onResponse(Call call, Response response);
     }
 
-    public void setSyncCallback(SyncCallback callback){
+    public void setSyncCallback(HttpCallback callback){
         this.callback = callback;
     }
 }
